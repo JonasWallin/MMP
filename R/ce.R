b36a2d8891c56b06cbef66507e91dd3a960d19ce
Yvette Baurne <yvette.baurne@gmail.com>
2020-11-06 15:25:25 +0100

    wrapper function for running model

---
+++ R/ce.R
@@ -0,0 +1,66 @@
+### wrapping function for analysis
+
+
+ce <- function(formula1, 
+               formula2, 
+               formula3, 
+               emergence, 
+               method,
+               time = NULL,
+               data) {
+  
+  data <- getData(formula1, 
+                  formula2, 
+                  formula3, 
+                  emergence, 
+                  method,
+                  time,
+                  data)
+  
+  
+  model <- list(list(formula1,data$names[1]), 
+                list(formula2,data$names[2]), 
+                list(formula3,data$names[3]), 
+                list(emergence,data$names[4]),
+                method,
+                time)
+  
+  names(model) <- c("Fixed effects", 
+                    "Individual random effects", 
+                    "Team random effects",
+                    "Emergence model",
+                    "Method",
+                    "Time")
+  
+  
+  
+  object <- dataToObject(data)
+  
+  param <- param0(object)
+  
+  res <-optim(param, function(x){-loglik(x, object) })
+  
+  betas <-getbeta(res$par, object)
+  
+  cov_beta <- getCovbeta(res$par, object)
+  
+  paramList <- paramToList(res$par, object)
+  
+  ce_model <- list(model, 
+                   betas, 
+                   cov_beta, 
+                   paramList)
+  
+  names(ce_model) <- c("model",
+                       "betas", 
+                       "cov_beta", 
+                       "covariances")
+  
+  # create class
+    class(ce_model) <- "ce"
+
+    
+  return(ce_model)
+}
+
+
