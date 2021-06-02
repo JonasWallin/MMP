cc67ef2771176bf3fc552510ce9d2cb86b41dc12
Yvette Baurne <yvette.baurne@gmail.com>
2021-06-02 17:24:03 +0200

    data generating functions for simulation

--- tmp/sim_wip.R
+++ tmp/sim_wip.R
@@ -21,6 +21,50 @@ gendat_I <- function(l3n,l2n,l1n,
 }
 
 
+## Simulate type I consensus (CEM type)
+## with group GP
+
+gendat_IGP <- function(l3n,l2n,l1n,
+                     mu0,mu1, # fixed effects
+                     sp, # individual variance
+                     se, # error variance
+                     sigma, # standard deviation GP
+                     corr, # correlation largest distance between time points
+                     delta1, # consensus
+                     delta2 # group delta
+                     ){
+  dat=expand.grid(time = 0:(l1n-1),
+                  person = 1:l2n,
+                  group = 1:l3n)
+
+  time <- 0:(l1n-1)
+  Dist <- as.matrix(dist(time))
+
+  ## räkna ut kappa om vi vill ha correlation x mellan 
+  ## längsta avståndet i data
+  x <- 0.05 # correlation
+  k <- max(Dist)/(-log(x))
+  
+  param <- c(sigma, -log(k)) # exp(param[2] ) = 1/k 
+  Sigma <- OUcov(Dist, param) 
+  
+# mean vector
+  mean <- mu0 + mu1*time
+  
+  # var-cov
+  covs <- diag(exp(delta2*time))%*%Sigma%*%diag(exp(delta2*time)) # sigma i mvrnorm
+  
+  g <- MASS::mvrnorm(l3n, mean, covs)
+  gpdat <- data.frame(cbind("group"=rep(1:l3n,each=l1n),"time"=rep(time,l3n),"g"=as.vector(t(g))))
+  dat <- dplyr::left_join(dat,gpdat,by=c("group","time"))
+  
+  dat$p0 <- rep(rnorm(l3n*l2n,0,sd=sqrt(sp)), each=l1n)
+  dat$p<- rnorm(l3n*l2n*l1n,dat$p0+dat$g,sd=sqrt(se*exp(2*delta1*dat$time)))
+  
+  # return
+  return(dat)
+}
+
 ## generates ce type II
 gendat_II <- function(l3n,l2n,l1n,
                    mu0,mu1, # fixed effects
@@ -35,9 +79,55 @@ gendat_II <- function(l3n,l2n,l1n,
   dat$g1<-g[,1][dat[,3]]
   dat$g2<-g[,2][dat[,3]]
   dat$g <- dat$g1+dat$g2*dat$time
-  dat$p<- rnorm(l3n*l2n*l1n,dat$g,sd=sqrt(sp*exp(2*delta1*dat$time)))
+  dat$ep<- rep(rnorm(l3n*l2n,0,sd=sqrt(sp)),each=l1n)
+  dat$p <- dat$g+dat$ep*exp(delta1*dat$time)
   dat$y<- rnorm(l3n*l2n*l1n,dat$p,sd=sqrt(se))
 
+  # return
+  return(dat)
+}
+
+## Simulate type II consensus (CEI type)
+## with group GP
+
+gendat_IIGP <- function(l3n,l2n,l1n,
+                       mu0,mu1, # fixed effects
+                       sp, # individual variance
+                       se, # error variance
+                       sigma, # standard deviation GP
+                       corr, # correlation largest distance between time points
+                       delta1, # consensus
+                       delta2 # group delta
+){
+  dat=expand.grid(time = 0:(l1n-1),
+                  person = 1:l2n,
+                  group = 1:l3n)
+  
+  time <- 0:(l1n-1)
+  Dist <- as.matrix(dist(time))
+  
+  ## räkna ut kappa om vi vill ha correlation x mellan 
+  ## längsta avståndet i data
+  x <- 0.05 # correlation
+  k <- max(Dist)/(-log(x))
+  
+  param <- c(sigma, -log(k)) # exp(param[2] ) = 1/k 
+  Sigma <- OUcov(Dist, param) 
+  
+  # mean vector
+  mean <- mu0 + mu1*time
+  
+  # var-cov
+  covs <- diag(exp(delta2*time))%*%Sigma%*%diag(exp(delta2*time)) # sigma i mvrnorm
+  
+  g <- MASS::mvrnorm(l3n, mean, covs)
+  gpdat <- data.frame(cbind("group"=rep(1:l3n,each=l1n),"time"=rep(time,l3n),"g"=as.vector(t(g))))
+  dat <- dplyr::left_join(dat,gpdat,by=c("group","time"))
+  
+  dat$ep<- rep(rnorm(l3n*l2n,0,sd=sqrt(sp)),each=l1n)
+  dat$p <- dat$g+dat$ep*exp(delta1*dat$time)
+  dat$y<- rnorm(l3n*l2n*l1n,dat$p,sd=sqrt(se))
+  
   # return
   return(dat)
 }
@@ -61,6 +151,26 @@ data_II <- gendat_II(l3n=5,l2n=5,l1n=10,
                    se=0.1,
                    delta1=-0.25)
 
+# data type I ce + GP
+data_IGP <- gendat_IGP(l3n=5,l2n=5,l1n=4,
+                     mu0=3,mu1=0.01,
+                     sp=0.01,
+                     se=0.2,
+                     sigma=0.1, # standard deviation GP
+                     corr=0.05, # correlation largest distance between time points
+                     delta1=-0.25,
+                     delta2=-0.15)
+
+# data type II ce + GP
+data_IIGP <- gendat_IIGP(l3n=5,l2n=5,l1n=4,
+                     mu0=3,mu1=0.01,
+                     sp=0.2,
+                     se=0.01,
+                     sigma=0.1, # standard deviation GP
+                     corr=0.05, # correlation largest distance between time points
+                     delta1=-0.25,
+                     delta2=-0.15)
+
 ## ce type I, cei model
 cei_I <- ce(p ~ 1+time, 
           ~ 1 | person, 
@@ -71,17 +181,17 @@ cei_I <- ce(p ~ 1+time,
 
 summary.ce(cei_I)
 
-## ce type I, cem model 
-cem_I <- ce(p ~ 1+time, 
+## ce type I + GP, cem model 
+cem_IGP <- ce(p ~ 1+time, 
           ~ 1 | person, 
           ~ 1 + time | group, 
           emergence = ~ 1 + time, 
           method = "CEM", 
-          data = data_I)
+          data = data_IGP)
 
 # error in solve.default(t(L), y_i) : 
 # system is computationally singular: reciprocal condition number = 2.48091e-23 
-
+summary.ce(cem_IGP)
 
 # same analysis with nlme (works fine)
 library(nlme)
@@ -89,19 +199,28 @@ library(nlme)
 cem_lme_I <- lme(p~time, 
     random = list(group=pdLogChol(~time),
                   person=pdIdent(~1)),
-    data=data_I, weights=varExp(form =~time))
+    data=data_IGP, weights=varExp(form =~time))
 
 summary(cem_lme_I)
 
 ## ce type II, cei model
-cei_II <- ce(y ~ 1+time, 
+cei_IIGP <- ce(y ~ 1+time, 
             ~ 1 | person, 
             ~ 1 + time | group, 
             emergence = ~ -1 + time, 
             method = "CEI", 
-            data = data_II)
-summary.ce(cei_II)
-# get delta=-26.58804, something is off
+            data = data_IIGP)
+summary.ce(cei_IIGP)
+
+cei_IIGPh <- ce(y ~ 1+time, 
+               ~ 1 | person, 
+               ~ 1| group, 
+               emergence = ~ -1 + time, 
+               method = "CEI", 
+               method.team = "OU.homeostasis",
+               data = data_IIGP)
+summary.ce(cei_IIGP)
+
 
 cem_II <- ce(y ~ 1+time, 
             ~ 1 | person, 
