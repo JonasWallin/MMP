a1aea72d71deb47351675aa9e3baa62f55f8b7ff
JonasWallin <jonas.wallin81@gmail.com>
2024-08-27 15:48:46 +0200

    figure add

---
+++ Article/Other figures/Figure_different_patterns.R
@@ -0,0 +1,74 @@
+##
+# Creating figure for the two limiting CE patterns and a Gaussian processes
+#
+##
+graphics.off()
+library(ggplot2)
+library(MMP)
+set.seed(22)
+save.fig = F
+T_     <- 4
+delta = -0.2
+n.grid <- 500
+n.obs  <- 5
+Maternparam <- log(c(1,3,2.4)) #sigma, kappa, nu
+
+# creating grid for continous line, and observations location
+grid       <- seq(0,T_, length.out = n.grid)
+grid.obs   <- c(seq(1,n.grid, by=ceiling(n.grid/n.obs)), n.grid) #location of the grid points!
+Sigma.grid <- Materncov(d = as.matrix(dist(grid)), Maternparam)
+L.grid     <- t(chol(Sigma.grid))
+# create three different
+Matern_cov     <- MaternConsenus$new(1)
+Matern_input   <-  list(D =    grid,
+                        time = grid) 
+Sigma_con  <- Matern_cov$get_Cov(c(delta,Maternparam), Matern_input)
+
+Z1 <- rnorm(n.grid)
+Z2 <- rnorm(n.grid)
+
+
+# Plot GP
+L.con      <- t(chol(Sigma_con))
+x1 <-  L.con%*%Z1
+x2 <- L.con%*%Z2
+fig.data = data.frame(time = grid, lower.CI = -1.96*exp(delta*grid), upper.CI =1.96*exp(delta*grid),
+                      x1 = x1, x2 = x2)
+fig.obs <- data.frame(time = grid[grid.obs], x1 = x1[grid.obs], x2 = x2[grid.obs])
+pl1 <- ggplot(data=fig.data) + 
+  geom_line(aes(x=time,y=x1 ),size=1,alpha=0.3,col='red')+ geom_line(aes(x=time,y=x2 ),size=1,alpha=0.3,col='red')  +
+  geom_point(data=fig.obs,aes(x=time,y=x1 ),size=2.5,col='red',shape=15) +geom_point(data=fig.obs,aes(x=time,y=x2 ),size=2.5,col='red')+
+  geom_line(aes(x = time,  y = lower.CI),color='blue',size=1.5,linetype = "dashed")+
+  geom_line(aes(x = time, y = upper.CI),color='blue',size=1.5,linetype = "dashed")+
+  xlab("Time") + 
+  ylab("")
+print(pl1)
+
+# plot Het
+x1.het <-  exp(delta*grid)*Z1
+x2.het <- exp(delta*grid)*Z2
+fig.data = data.frame(time = grid, lower.CI = -1.96*exp(delta*grid), upper.CI =1.96*exp(delta*grid),
+                      x1 = x1.het, x2 = x2.het)
+fig.obs <- data.frame(time = grid[grid.obs], x1 = x1.het[grid.obs], x2 = x2.het[grid.obs])
+pl2 <- ggplot(data=fig.data) + 
+  geom_point(data=fig.obs,aes(x=time,y=x1 ),size=2.5,col='red',shape=15) +geom_point(data=fig.obs,aes(x=time,y=x2 ),size=2.5,col='red')+
+  geom_line(aes(x = time,  y = lower.CI),color='blue',size=1.5,linetype = "dashed")+
+  geom_line(aes(x = time, y = upper.CI),color='blue',size=1.5,linetype = "dashed")+
+  xlab("Time") + 
+  ylab("")
+print(pl2)
+
+# plot Hom
+x1.hom <-  exp(delta*grid)*Z1[1]
+x2.hom <- exp(delta*grid)*Z2[1]
+fig.data = data.frame(time = grid, lower.CI = -1.96*exp(delta*grid), upper.CI =1.96*exp(delta*grid),
+                      x1 = x1.hom, x2 = x2.hom)
+fig.obs <- data.frame(time = grid[grid.obs], x1 = x1.hom[grid.obs], x2 = x2.hom[grid.obs])
+pl3 <- ggplot(data=fig.data) + 
+  geom_line(aes(x=time,y=x1 ),size=1,alpha=0.3,col='red')+ geom_line(aes(x=time,y=x2 ),size=1,alpha=0.3,col='red')  +
+  geom_point(data=fig.obs,aes(x=time,y=x1 ),size=2.5,col='red',shape=15) +geom_point(data=fig.obs,aes(x=time,y=x2 ),size=2.5,col='red')+
+  geom_line(aes(x = time,  y = lower.CI),color='blue',size=1.5,linetype = "dashed")+
+  geom_line(aes(x = time, y = upper.CI),color='blue',size=1.5,linetype = "dashed")+
+  xlab("Time") + 
+  ylab("")
+print(pl3)
